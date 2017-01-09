9c9300b4880eb0843f3a67725f3cf70e2d962507
Vahid D <vahidnety@gmail.com>
2017-01-09 17:21:38 +0800

    initial

--- staffFinder/AppDelegate.m
+++ staffFinder/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    //Override point for customization after application launch.
     if ([CLLocationManager locationServicesEnabled]) {
         if (!_locationManager) {
             _locationManager = [[CLLocationManager alloc] init];
